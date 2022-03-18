8a90769a2d77e90aced7a9f33b058d0873b08f41
임광훈 <hooeni@gmail.com>
2022-03-18 17:44:13 +0900

    고정아이피 추가

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h8mbnoctobo-nqytn0*dj(e*a1_xa7-8lpxwoamd*k@+vo8v+!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.79.208.240']
 
 
 # Application definition
